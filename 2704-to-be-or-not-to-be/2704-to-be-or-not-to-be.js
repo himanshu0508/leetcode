20ed654c1cbd7e40de81557ee0383620e282d031
HIMANSHU <161239730+himanshu0508@users.noreply.github.com>
2026-06-23 22:46:31 +0530

    Time: 37 ms (89.09%), Space: 53.2 MB (80.9%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(other) {
+            if (val === other) {
+                return true;
+            }
+            throw new Error("Not Equal");
+        },
+        notToBe: function(other) {
+            if (val !== other) {
+                return true;
+            }
+            throw new Error("Equal");
+        }
+    };
+};
