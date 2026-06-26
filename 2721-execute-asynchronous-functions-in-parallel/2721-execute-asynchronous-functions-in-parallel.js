fc9556e0f1efcd318952475652d3bc006c21e3ac
HIMANSHU <161239730+himanshu0508@users.noreply.github.com>
2026-06-26 23:16:13 +0530

    Time: 60 ms (70.09%), Space: 56 MB (5.4%) - LeetHub

---
+++ 2721-execute-asynchronous-functions-in-parallel/2721-execute-asynchronous-functions-in-parallel.js
@@ -0,0 +1,28 @@
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+var promiseAll = function(functions) {
+    return new Promise((resolve, reject) => {
+        const results = [];
+        let completed = 0;
+
+        if (functions.length === 0) {
+            resolve(results);
+            return;
+        }
+
+        functions.forEach((fn, index) => {
+            fn()
+                .then(result => {
+                    results[index] = result;
+                    completed++;
+
+                    if (completed === functions.length) {
+                        resolve(results);
+                    }
+                })
+                .catch(reject);
+        });
+    });
+};
